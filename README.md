8f15aca36b5e15752bbab4b35c3797d0741a576d
amita125 <amitaghale@gmail.com>
2020-05-18 12:51:03 +0100

    2 different database created with seedfile

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # habit-app
-<<<<<<< HEAD
-=======
 
-`npm install `
->>>>>>> 1c930bcdcb0fb5777b828c71a0d631310701d8f4
+`npm install`
--- habitdb/migrations/20200518120303_userTable.js
+++ habitdb/migrations/20200518120303_userTable.js
@@ -1,8 +1,8 @@
-exports.up = function (knex) {
+exports.up = function (knex, Promise) {
   return knex.schema.createTable("userTable", (table) => {
-    table.increments("id").primary();
+    table.increments("user_id").primary();
     table.string("name", 256).notNullable();
-    table.string("surname", 128).notNullable();
+    table.string("surname", 256).notNullable();
     table.string("username", 128).notNullable();
   });
 };
