6d1404281869a6e37213d2c175ca3ec03f2a549e
amita125 <amitaghale@gmail.com>
2020-05-18 10:54:43 +0100

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+module.exports = {
+  test: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "recipes_test",
+    },
+    migrations: {
+      directory: __dirname + "/habitdb/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/habitdb/seeds/test",
+    },
+  },
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: "recipes",
+    },
+    migrations: {
+      directory: __dirname + "/habitdb/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/habitdb/seeds/development",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    migrations: {
+      directory: __dirname + "/habitdb/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/habitdb/seeds/production",
+    },
+  },
+};
