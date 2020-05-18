7af9480ffce1e7ac36f9d8c5b2fcf00ccf408b81
amita125 <amitaghale@gmail.com>
2020-05-18 14:45:12 +0100

    react file added

---
+++ habitapp/src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <nav className="navbar" id="mainNav">
+        <button type="button" className="btn btn-outline-dark">
+          <Link to="/">Habit Tracker</Link>
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ habitapp/src/Components/Register.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+export class Register extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Register;
