62cb915afea6d6be45a01df2905022f25759ef28
amita125 <amitaghale@gmail.com>
2020-05-19 17:37:59 +0100

    habit being added and displayed in the frontend from backend

---
+++ habitapp/src/Components/ViewHabit.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+class ViewHabit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      habitData: [],
+      complete: false,
+    };
+  }
+  componentDidMount() {
+    fetch(`/habitapi/${this.props.user}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ habitData: data });
+      });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.habitData ? (
+          this.state.habitData.map((object) => (
+            <div>
+              {object.habitName}
+              {object.frequency}
+              {object.complete}{" "}
+            </div>
+          ))
+        ) : (
+          <h1>Data logged in </h1>
+        )}
+      </div>
+    );
+  }
+}
+
+export default ViewHabit;
