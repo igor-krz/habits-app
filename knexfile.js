be5f7559b54145f524dfc167448f56ab7a3df4b4
fatima03598 <54324041+fatima03598@users.noreply.github.com>
2020-05-20 17:00:02 +0100

    checkbox

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,29 @@
 require("dotenv").config();
 module.exports = {
-  test: {
-    client: "pg",
-    connection: {
-      host: "localhost",
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
+  // test: {
+  //   client: "pg",
+  //   connection: {
+  //     host: "localhost",
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PASS,
 
-      database: "habitdb_test",
+  //     database: "habitdb_test",
 
-    },
-    migrations: {
-      directory: __dirname + "/habitdb/migrations",
-    },
-    seeds: {
-      directory: __dirname + "/habitdb/seeds/test",
-    },
-  },
+  //   },
+  //   migrations: {
+  //     directory: __dirname + "/habitdb/migrations",
+  //   },
+  //   seeds: {
+  //     directory: __dirname + "/habitdb/seeds/test",
+  //   },
+  // },
   development: {
     client: "pg",
     connection: {
       host: "localhost",
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: "habitdb",
+      database: "habitdbmain",
     },
     migrations: {
       directory: __dirname + "/habitdb/migrations",
@@ -32,18 +32,18 @@ module.exports = {
       directory: __dirname + "/habitdb/seeds/development",
     },
   },
-  production: {
-    client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    migrations: {
-      directory: __dirname + "/habitdb/migrations",
-    },
-    seeds: {
-      directory: __dirname + "/habitdb/seeds/production",
-    },
-  },
+  // production: {
+  //   client: "pg",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   migrations: {
+  //     directory: __dirname + "/habitdb/migrations",
+  //   },
+  //   seeds: {
+  //     directory: __dirname + "/habitdb/seeds/production",
+  //   },
+  // },
 };
