68f80f807026c7efae53fbd6558ac2a08ec9e29d
amita125 <amitaghale@gmail.com>
2020-05-21 09:37:05 +0100

    changes merged

--- habitapp/src/Containers/Dashboard.js
+++ habitapp/src/Containers/Dashboard.js
@@ -7,51 +7,49 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     date:'',
-     dayWeek:'',
-     weekday:['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      date: "",
+      dayWeek: "",
+      weekday: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
     };
   }
 
-
-
   componentDidMount() {
-    const date = new Date()
-    let day = date.getDate()
-    let month = date.getMonth()+1
-    let year = date.getFullYear()
-    let newdate = day + '-' + month + '-' + year
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let newdate = day + "-" + month + "-" + year;
     let dayWeek = this.state.weekday[date.getDay()];
     this.setState({
       date: newdate,
-      dayWeek:dayWeek
-
-    }
-    )
+      dayWeek: dayWeek,
+    });
   }
   render() {
-   const { user_id, userName, userSurname} = this.props.location.state.data
-   const {dayWeek, date}  = this.state
+    const { user_id, userName, userSurname } = this.props.location.state.data;
+    const { dayWeek, date } = this.state;
     return (
       <div className="container-fluid" id="HomePage">
         <h1>{dayWeek}</h1>
-    <h3>Welcome:  {userName} {userSurname}</h3>
-
+        <h3>
+          Welcome: {userName} {userSurname}
+        </h3>
 
         <div className="row">
           <div className="col-lg-2" id="asideArea"></div>
           <div className="col-lg-8">
             <div className="App-body">
-<<<<<<< HEAD
-              <HabitCheckForm user={this.props.location.state.data} />
-              <NewHabit user={this.props.location.state.data} />
-              <ViewHabit user={this.props.location.state.data} />
-              <Habit userID={this.props.location.state.data} />
-=======
-              <HabitCheckForm user={user_id}  date={date}  /> 
-              <NewHabit user={user_id}   date={date} />
-              <ViewHabit user={user_id}   date={date} />
->>>>>>> d7955d5e0b8aa51edddffa97d07e2a9d4e69e339
+              <HabitCheckForm user={user_id} date={date} />
+              <NewHabit user={user_id} date={date} />
+              <ViewHabit user={user_id} date={date} />
             </div>
           </div>
           <div className="col-lg-2" id="asideArea"></div>
--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 module.exports = {
-  // test: {
-  //   client: "pg",
-  //   connection: {
-  //     host: "localhost",
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASS,
+  test: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
 
-  //     database: "habitdb_test",
-
-  //   },
-  //   migrations: {
-  //     directory: __dirname + "/habitdb/migrations",
-  //   },
-  //   seeds: {
-  //     directory: __dirname + "/habitdb/seeds/test",
-  //   },
-  // },
+      database: "habitdb_test",
+    },
+    migrations: {
+      directory: __dirname + "/habitdb/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/habitdb/seeds/test",
+    },
+  },
   development: {
     client: "pg",
     connection: {
@@ -32,18 +31,18 @@ module.exports = {
       directory: __dirname + "/habitdb/seeds/development",
     },
   },
-  // production: {
-  //   client: "pg",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password",
-  //   },
-  //   migrations: {
-  //     directory: __dirname + "/habitdb/migrations",
-  //   },
-  //   seeds: {
-  //     directory: __dirname + "/habitdb/seeds/production",
-  //   },
-  // },
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    migrations: {
+      directory: __dirname + "/habitdb/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/habitdb/seeds/production",
+    },
+  },
 };
